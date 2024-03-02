dcff6ed734ab7e0e71bf920db5ae06629a8a9d08
IamKaki <rizkarize@gmail.com>
2024-03-02 22:57:08 +0300

    This is for expaning from a smaller data type toa bigger data type

---
+++ widecasting.java
@@ -0,0 +1,9 @@
+public class widecasting {
+    public static void main(String[] args){
+        int a = 33;
+        float b = a;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
