b32d09089365ee396d0a4352d176c626df3b6446
IamKaki <rizkarize@gmail.com>
2024-03-02 20:01:30 +0300

    Learnt how to display text plus variables in a program

---
+++ displayTextVariable.java
@@ -0,0 +1,11 @@
+public class displayTextVariable {
+    public static void main(String[] args){
+        String firstname = "John ";
+        String secondname = "Kaki";
+        String fullname = firstname + secondname;
+        int Age = 30;
+        System.out.print("Hello? My names are " + fullname);
+        System.out.print(" and I am " + Age);
+        System.out.println(" of age.");
+    }
+}
