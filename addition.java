dc1907fb8d638404574623375cbb7f1a0b0b2ab3
IamKaki <rizkarize@gmail.com>
2024-03-02 19:58:47 +0300

    Learnt to do additions

---
+++ addition.java
@@ -0,0 +1,8 @@
+public class addition {
+
+    public static void main(String[] args){
+        System.out.println(3 * 3);
+        System.out.println(4 * 4);
+        System.out.println(5 * 5);
+    }
+}
