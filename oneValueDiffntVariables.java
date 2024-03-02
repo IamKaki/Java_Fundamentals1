a8c6d3096a17e50f7a5c60e18e55739f4087f697
IamKaki <rizkarize@gmail.com>
2024-03-02 21:34:01 +0300

    Assigning one value to different variables in a single statement

---
+++ oneValueDiffntVariables.java
@@ -0,0 +1,8 @@
+public class oneValueDiffntVariables {
+    public static void main(String[] args){
+        int z, x, c, v;
+        z = x = c = v = 70;
+
+        System.out.println(z + x + c + v);
+    }
+}
