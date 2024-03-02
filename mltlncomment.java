ad61685183df971a3f4fcbde1ef2d2a2f16b38ff
IamKaki <rizkarize@gmail.com>
2024-03-02 20:03:00 +0300

    Learnt about multiline comments in java

---
+++ mltlncomment.java
@@ -0,0 +1,8 @@
+public class mltlncomment {
+    public static void main(String[] args){
+        /*Be brave Kakiman, all will be well. Today is the 2nd of march 2024 and u have 
+        earnestly begun ur pursuit of a better life. U now have the opportunity to change
+        and start afresh. Keep the fire burning and do ur best always.*/
+        System.out.println("There is a message in the multiline comment in this program!");
+    }
+}
