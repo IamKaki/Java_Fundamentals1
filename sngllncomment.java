b1c909b1080757b650549ddb59577e3fc7e88067
IamKaki <rizkarize@gmail.com>
2024-03-02 20:03:54 +0300

    Learnt about singleline comments in java

---
+++ sngllncomment.java
@@ -0,0 +1,6 @@
+public class sngllncomment {
+    public static void main(String[] args){
+        System.out.println("This very line has a comment, but u cant see it."); // this is a comment
+    }
+    
+}
