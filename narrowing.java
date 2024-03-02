fd2b9e907115a6e601dfaf256256575b753be6b1
IamKaki <rizkarize@gmail.com>
2024-03-02 22:55:47 +0300

    This is for narrowing from a bigger data type toa smaller data type

---
+++ narrowing.java
@@ -0,0 +1,9 @@
+public class narrowing {
+    public static void main(String[] args){
+        float b = 3.146f;
+        int z = (int) b;
+
+        System.out.println(b);
+        System.out.println(z);
+    }
+}
