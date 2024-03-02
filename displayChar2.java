ff59aafdd874c15c54b3456dcdfec2a79bfb5c4a
IamKaki <rizkarize@gmail.com>
2024-03-02 21:32:18 +0300

    Displaying ASCII Character values

---
+++ displayChar2.java
@@ -0,0 +1,9 @@
+public class displayChar2 {
+    public static void main(String[] args){
+        char var1 = 65, var2 = 66, var3 = 67;
+
+        System.out.println("My grade for Chemistry is " + var1);
+        System.out.println("My grade for Kiswahili is " + var2);
+        System.out.println("My garde for English is " + var3);
+    }
+}
