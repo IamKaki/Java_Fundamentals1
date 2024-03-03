87ab86ba1569e34dbbfd6537960f9baa9d490f4f
IamKaki <rizkarize@gmail.com>
2024-03-03 14:01:24 +0300

    Introduction to if conditional statement

---
+++ ifV.java
@@ -0,0 +1,10 @@
+public class ifV {
+    public static void main(String[] args){
+        if (10 > 100){
+            System.out.println("10 is greater than 100.");
+        }
+        else{
+            System.out.println("10 is not greater than 100.");
+        }
+    }
+}
