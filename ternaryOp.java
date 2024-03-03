b1bf0522b55fbdd608bca21ae3350b43b3448a73
IamKaki <rizkarize@gmail.com>
2024-03-03 14:02:39 +0300

    Introduction to ternary conditional statement, whic is used in place of if-else statements

---
+++ ternaryOp.java
@@ -0,0 +1,7 @@
+public class ternaryOp {
+    public static void main(String[] args){
+        int age = 13;
+        String result = (age > 18)? "You are old enough." : "You are too young";
+        System.out.println(result);
+    }
+}
