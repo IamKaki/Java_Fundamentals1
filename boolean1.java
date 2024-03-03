90a76785d26693d98115dcc1b4f2e3f0733cecae
IamKaki <rizkarize@gmail.com>
2024-03-03 13:59:41 +0300

    Boolean expression combined with an if-elseif conditional statement

---
+++ boolean1.java
@@ -0,0 +1,13 @@
+public class boolean1 {
+    public static void main(String[] args){
+        int myAge = 13;
+        int votingAge = 18;
+
+        if (myAge >= 18){
+            System.out.println("You are eligible to vote!");
+        }
+        else{
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+}
