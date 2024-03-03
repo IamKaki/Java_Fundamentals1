d4189681d83da41dcf2736e34807825eaf368a84
IamKaki <rizkarize@gmail.com>
2024-03-03 09:25:52 +0300

    learnt how to generate random numbers in Java

---
+++ randomNumberGenerator.java
@@ -0,0 +1,7 @@
+public class randomNumberGenerator {
+    public static void main(String[] args){
+        int randomnumber = (int)(Math.random() * 6);
+
+        System.out.println(randomnumber);
+    }
+}
