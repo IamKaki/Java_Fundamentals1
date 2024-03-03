3dbd59ed26af03c4d89d87c1259ccaefc1682b35
IamKaki <rizkarize@gmail.com>
2024-03-03 09:22:53 +0300

    Getting the exact position of a character or white space in a string

---
+++ locatingIndexes.java
@@ -0,0 +1,7 @@
+public class locatingIndexes {
+    public static void main(String[] args){
+        String sentence = "I am Kakiman";
+
+        System.out.println(sentence.indexOf("k"));
+    }
+}
