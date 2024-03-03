7de76362df7bb5e48de0067cbca9a83f6148f31b
IamKaki <rizkarize@gmail.com>
2024-03-03 09:27:08 +0300

    learnt how to combine two or more string variables

---
+++ stingConcntination.java
@@ -0,0 +1,8 @@
+public class stingConcntination {
+    public static void main(String[] args){
+        String firstname = "John ";
+        String secondname = "Kakiman";
+
+        System.out.println(firstname.concat(secondname));
+    }
+}
