0b0dc4762b19c19cf1e89199b45525276c77114f
IamKaki <rizkarize@gmail.com>
2024-03-03 20:19:43 +0300

    into to switch case method

---
+++ switchcase.java
@@ -0,0 +1,25 @@
+public class switchcase {
+    public static void main(String[] args){
+        int day = 4;
+
+        switch (day) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+           /* case 4:
+                System.out.println("Wednesday");
+                break; */
+            case 5:
+                System.out.println("Thursday");
+                break;
+            default:
+                System.out.println("Weekend");
+        }
+    }
+}
