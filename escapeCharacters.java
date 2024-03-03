b8b3e7d805418358f17f53f3f97178b15c5c9b89
IamKaki <rizkarize@gmail.com>
2024-03-03 09:24:19 +0300

    Learnt about escape characters in Java

---
+++ escapeCharacters.java
@@ -0,0 +1,7 @@
+public class escapeCharacters {
+    public static void main(String[] args){
+        String vikings = "They are the so called \"Vikings\" from the north.";
+
+        System.out.println(vikings);
+    }
+}
