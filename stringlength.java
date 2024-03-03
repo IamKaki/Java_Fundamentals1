e5f56851bea54ef3d4ff81ddbc2ccf149f66dc88
IamKaki <rizkarize@gmail.com>
2024-03-03 09:28:06 +0300

    learnt how to count the length of a string variable

---
+++ stringlength.java
@@ -0,0 +1,7 @@
+public class stringlength {
+    public static void main(String[] args){
+        String txt = "ABCDEFGHIJKLMNOPQRUSTVWXYZ";
+
+        System.out.println("The length of the string txt is " + txt.length());
+    }
+}
