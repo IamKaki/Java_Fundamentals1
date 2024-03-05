03aa45a84bc9963e91cf3782cc925677f20cc6ed
IamKaki <rizkarize@gmail.com>
2024-03-06 01:45:51 +0300

    Using the break function on a for loop

---
+++ break1.java
@@ -0,0 +1,10 @@
+public class break1 {
+    public static void main(String[] args){
+        for (int i = 1; i <= 10; i++){
+            if (i == 8){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
