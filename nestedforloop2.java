ab7d367bce84e1206dd104c7a8b77c9bcd63c86e
IamKaki <rizkarize@gmail.com>
2024-03-05 08:28:02 +0300

    Used for loops to ittarate a function in java

---
+++ nestedforloop2.java
@@ -0,0 +1,10 @@
+public class nestedforloop2 {
+    public static void main(String[] args){
+        for (int i = 1; i <= 2; i++){
+            System.out.println("Outer: " + i);
+            for (int j = 1; j <= 3; j++){
+                System.out.println(" Inner: " + j);
+            }
+        }
+    }
+}
