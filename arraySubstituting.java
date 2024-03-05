d6c4ac698843c8218aa259ab8739678d8b8b8791
IamKaki <rizkarize@gmail.com>
2024-03-06 02:32:55 +0300

    Substituting elements with new ones in an array in Java

---
+++ arraySubstituting.java
@@ -0,0 +1,7 @@
+public class arraySubstituting {
+    public static void main(String[] args){
+        String[] cars = {"Toyota", "Audi", "BMW", "VW"};
+        cars[0] = "Benz";
+        System.out.println(cars[0]);
+    }
+}
---
+++ arraySubstituting2.java
@@ -0,0 +1,7 @@
+public class arraySubstituting2 {
+    public static void main(String[] args){
+        int[] myNumb = {12, 33, 13, 18, 88};
+       // myNumb[1] = 99;
+        System.out.println(myNumb[1]);
+    }
+}
