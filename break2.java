fac3a39a0618c8f66c95665fdb2d630710a90348
IamKaki <rizkarize@gmail.com>
2024-03-06 01:46:25 +0300

    Using the break function on a while loop

---
+++ break2.java
@@ -0,0 +1,12 @@
+public class break2 {
+    public static void main(String[] args){
+        int i = 1;
+        while (i <= 10) {
+            if (i == 7) {
+                break;                
+            }
+            System.out.println(i);
+            i++;
+        }
+    }
+}
