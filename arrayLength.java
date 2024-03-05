032b755a2140cc4968a6c8dd42f809e609407240
IamKaki <rizkarize@gmail.com>
2024-03-06 02:31:14 +0300

    Displaying how many elements are in an array in Java

---
+++ arrayLength.java
@@ -0,0 +1,6 @@
+public class arrayLength {
+    public static void main(String[] args){
+        String[] cities = {"Daban", "Jobarg", "Harare", "Addis", "Kampala", "Arusha", "Nakuru"};
+        System.out.println(cities[3]);
+    }
+}
