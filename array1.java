fc101ecec5acee20642b35217751ce0767f837d5
IamKaki <rizkarize@gmail.com>
2024-03-06 02:29:04 +0300

    Declaring Arrays in Java

---
+++ array1.java
@@ -0,0 +1,6 @@
+public class array1 {
+    public static void main(String[] args){
+        String[] cars = {"Benz", "Volvo", "Audi", "BMW", "VoxW"};
+        System.out.println(cars [0]);
+    }
+}
