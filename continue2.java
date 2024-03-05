b7fd84d1b2e4168a5ae938ac6b0b316d56658c37
IamKaki <rizkarize@gmail.com>
2024-03-06 01:47:39 +0300

    Using the continue function on a while loop

---
+++ continue2.java
@@ -0,0 +1,13 @@
+public class continue2 {
+    public static void main(String[] args){
+        int i = 1;
+        while (i <= 10) {
+            if (i == 7) {
+                i++;
+                continue;
+            }
+            System.out.println(i);
+            i++;
+        }
+    }
+}
