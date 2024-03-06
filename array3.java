15a38dc3b5bafd4efae9a220d7d23ef033d9f6c2
IamKaki <rizkarize@gmail.com>
2024-03-06 04:10:30 +0300

    Having fun with Arrays in Java

---
+++ array3.java
@@ -0,0 +1,8 @@
+public class array3 {
+    public static void main(String[] args){
+        String[] cars = { "Benz", "BMW", "Volvo", "VW", "AUDI", "Porsche", "Opel", "Seat", "Skoda"};
+        for (String i : cars){
+        System.out.print(i + " ");
+        }
+    }
+}
