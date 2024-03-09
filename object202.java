247728799f577ea58a21308d0958a53e9e4dd893
IamKaki <rizkarize@gmail.com>
2024-03-09 16:31:30 +0300

    Practice  with creating objects in Java

---
+++ object202.java
@@ -0,0 +1,12 @@
+public class object202 {
+    int x = 7;
+    public static void main(String[] args) {
+        object202 myObject1 = new object202();
+        object202 myObject2 = new object202();
+        object202 myObject3 = new object202();
+
+        System.out.println(myObject1.x);
+        System.out.println(myObject2.x);
+        System.out.println(myObject3.x);
+    }
+}
