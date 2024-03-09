3e92d57b5dd7ae3296983e60d5182a53263c66be
IamKaki <rizkarize@gmail.com>
2024-03-09 16:58:32 +0300

    Modifying already created attributes in the class through objects

---
+++ Indepth1/modifyAtri.java
@@ -0,0 +1,9 @@
+public class modifyAtri {
+    int x;
+    public static void main(String[] args) {
+        modifyAtri myObj = new modifyAtri();
+        myObj.x = 10;
+
+        System.out.println(myObj.x);
+    }
+}
