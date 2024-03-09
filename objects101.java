20aafec1f2da7e50e5fcdf258f726c3980350f42
IamKaki <rizkarize@gmail.com>
2024-03-09 16:30:46 +0300

    First taste with creating objects in Java

---
+++ objects101.java
@@ -0,0 +1,7 @@
+public class objects101 {
+    int x = 5;
+    public static void main(String[] args) {
+        objects101 myObject = new objects101();
+        System.out.println(myObject.x);
+    }
+}
