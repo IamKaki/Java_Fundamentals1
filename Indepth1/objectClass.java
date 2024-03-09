24917435b034830d3bc3f62d2bc815f3662acc54
IamKaki <rizkarize@gmail.com>
2024-03-09 16:57:10 +0300

    Created a class and an object inside it

---
+++ Indepth1/objectClass.java
@@ -0,0 +1,8 @@
+public class objectClass {
+    int x = 10;
+    public static void main(String[] args) {
+        objectClass myObj = new objectClass();
+
+        System.out.println(myObj.x);
+    }
+}
