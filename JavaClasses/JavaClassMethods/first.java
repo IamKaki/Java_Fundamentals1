8a07fe7365ba3db0983809ab3b6f106e126d696d
IamKaki <rizkarize@gmail.com>
2024-03-10 22:45:34 +0300

    More practice on creating methods in one file and objects to call them in another file

---
+++ JavaClasses/JavaClassMethods/first.java
@@ -0,0 +1,8 @@
+public class first {
+    public void Method(){
+        System.out.println("This is method 1.");
+    }  
+    public void Method2(){
+        System.out.println("This is method 2.");
+    }
+}
