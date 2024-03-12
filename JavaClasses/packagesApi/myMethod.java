125e9f0c8820fad25da3279b0ef0c6b77ab7ca25
IamKaki <rizkarize@gmail.com>
2024-03-13 00:01:33 +0300

    More practice on methods

---
+++ JavaClasses/packagesApi/myMethod.java
@@ -0,0 +1,8 @@
+public class myMethod{
+    static void myMethod1(){
+        System.out.println("I just got executed");
+    }
+    public static void main(String[] args) {
+        myMethod1();
+    }
+}
